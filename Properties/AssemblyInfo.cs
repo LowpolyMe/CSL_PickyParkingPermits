bba2113fa87709e4ee9cf39cbfd8ffa59d9772c1
Sarah (LowPolyMe) <lowpolyme@web.de>
2025-12-18 17:07:31 +0100

    repo setup

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSL_PickyParkingPermits")]
+[assembly: AssemblyDescription("temp: allows ")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lowpolyme")]
+[assembly: AssemblyProduct("CSL_PickyParkingPermits")]
+[assembly: AssemblyCopyright("Copyright © Lowpolyme 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("502ACBFA-350D-4F8D-9599-3CD371EF3254")]
+
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
